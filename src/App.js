baea524761078a95f6099360d863dbc54cb7895e
VINAY R NITTALI <vinayvrn12@gmail.com>
2021-10-02 19:31:36 +0530

    Added sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 
 function App() {
     return (
       <div className="App">
           <Topbar />
+          <div className='container'>
+            <Sidebar />
+          </div>
       </div>
     );
   }
