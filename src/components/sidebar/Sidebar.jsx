33881527cc0c7893965dabb72b424d8ea2db97f1
VINAY R NITTALI <vinayvrn12@gmail.com>
2021-10-03 13:11:44 +0530

    Added chart

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -8,7 +8,7 @@ const Sidebar = () => {
                 <div className="sidebarMenu">
                     <h3 className="sidebarTitle">Dashboard</h3>
                     <ul className="sidebarList">
-                        <li className="sidebarListItem">
+                        <li className="sidebarListItem active">
                             <LineStyle className='sidebarIcon'/>
                             Home
                         </li>
